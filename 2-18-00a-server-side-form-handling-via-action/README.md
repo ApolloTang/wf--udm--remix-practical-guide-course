1e11aef4cc2240e7f9e4ca1dee137ade374e2ac9
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-03-10 22:52:57 -0500

    done: 2-18-00a-server-side-form-handling-via-action

---
+++ 2-18-00a-server-side-form-handling-via-action/README.md
@@ -0,0 +1,56 @@
+# 18 Server side form handling via action function
+
+*file: / app/route/notes.jsx*
+
+```jsx
+export async function action({ request }) {
+  console.dir(request, {showHidden: false, depth: 2, colors: false});
+
+  return null
+}
+```
+
+
+
+Console output:
+
+```
+  NodeRequest [Request] {
+    size: 0,
+    follow: 20,
+    compress: true,
+    counter: 0,
+    agent: undefined,
+    highWaterMark: 16384,
+    insecureHTTPParser: false,
+    [Symbol(Body internals)]: {
+      body: ReadableStream {
+        _state: 'readable',
+        _reader: undefined,
+        _storedError: undefined,
+        _disturbed: false,
+        _readableStreamController: [ReadableStreamDefaultController]
+      },
+      type: null,
+      size: null,
+      boundary: null,
+      disturbed: false,
+      error: null
+    },
+    [Symbol(Request internals)]: {
+      method: 'POST',
+      redirect: 'follow',
+      headers: Headers { [Symbol(query)]: [Array], [Symbol(context)]: null },
+      credentials: 'same-origin',
+      parsedURL: URL {
+        [Symbol(context)]: [URLContext],
+        [Symbol(query)]: [URLSearchParams]
+      },
+      signal: AbortSignal {}
+    }
+  }
+  POST /notes 200 - - 11.767 ms
+```
+
+
+
---
+++ 2-18-00a-server-side-form-handling-via-action/app/routes/notes.jsx
@@ -0,0 +1,58 @@
+import NewNote, { links as newNoteLinks } from '~/components/NewNote';
+
+export default function NotesPage() {
+  return (
+    <main>
+      <NewNote />
+    </main>
+  );
+}
+
+export async function action({ request }) {
+  console.dir(request, {showHidden: false, depth: 2, colors: false});
+
+  /*
+  GET /notes 200 - - 10.771 ms
+  NodeRequest [Request] {
+    size: 0,
+    follow: 20,
+    compress: true,
+    counter: 0,
+    agent: undefined,
+    highWaterMark: 16384,
+    insecureHTTPParser: false,
+    [Symbol(Body internals)]: {
+      body: ReadableStream {
+        _state: 'readable',
+        _reader: undefined,
+        _storedError: undefined,
+        _disturbed: false,
+        _readableStreamController: [ReadableStreamDefaultController]
+      },
+      type: null,
+      size: null,
+      boundary: null,
+      disturbed: false,
+      error: null
+    },
+    [Symbol(Request internals)]: {
+      method: 'POST',
+      redirect: 'follow',
+      headers: Headers { [Symbol(query)]: [Array], [Symbol(context)]: null },
+      credentials: 'same-origin',
+      parsedURL: URL {
+        [Symbol(context)]: [URLContext],
+        [Symbol(query)]: [URLSearchParams]
+      },
+      signal: AbortSignal {}
+    }
+  }
+  POST /notes 200 - - 11.767 ms
+  */
+
+  return null
+}
+
+export function links() {
+  return [...newNoteLinks()];
+}
